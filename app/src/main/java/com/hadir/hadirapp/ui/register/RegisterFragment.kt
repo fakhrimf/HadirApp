ca26725e83d4157d64da4fc6bbcf72e359920c95
fakhrimf <fakhrimf23@gmail.com>
2020-02-06 08:42:45 +0700

    update RegisterFragment()

--- app/src/main/java/com/hadir/hadirapp/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/hadir/hadirapp/ui/register/RegisterFragment.kt
@@ -2,17 +2,11 @@ package com.hadir.hadirapp.ui.register
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.hadir.hadirapp.LoginActivity
 import com.hadir.hadirapp.R
-import com.hadir.hadirapp.model.TeacherModel
+import com.hadir.hadirapp.login.LoginActivity
 import com.hadir.hadirapp.ui.base.BaseFragment
 import kotlinx.android.synthetic.main.fragment_register.*
 
@@ -30,13 +24,9 @@ class RegisterFragment : BaseFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
         loginButton.setOnClickListener {
             val intent = Intent(requireContext(), LoginActivity::class.java)
             startActivity(intent)
-
         }
-
-
     }
 }
