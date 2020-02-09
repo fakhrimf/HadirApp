ec841b4ba8a71cf4954eed3f4cba10eba00d5435
alvirizqi0@gmail.com <alvirizqi0@gmail.com>
2020-02-09 13:03:14 +0700

    Add drawer item for all fragment, fix constraint activity_login

--- app/src/main/java/com/hadir/hadirapp/HomeActivity.kt
+++ app/src/main/java/com/hadir/hadirapp/HomeActivity.kt
@@ -2,24 +2,43 @@ package com.hadir.hadirapp
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.annotation.DrawableRes
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
+import com.mikepenz.materialdrawer.widget.AccountHeaderView
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 import com.mikepenz.materialdrawer.holder.ColorHolder
 import com.mikepenz.materialdrawer.holder.StringHolder
 import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.widget.MaterialDrawerSliderView
+import com.mikepenz.materialdrawer.holder.ImageHolder
+import com.mikepenz.materialdrawer.interfaces.ICrossfader
 import com.mikepenz.materialdrawer.model.SecondaryDrawerItem
+import com.mikepenz.materialdrawer.model.interfaces.withEmail
+import com.mikepenz.materialdrawer.model.interfaces.withIcon
+import com.mikepenz.materialdrawer.model.interfaces.withIdentifier
+import com.mikepenz.materialdrawer.model.interfaces.withName
+import com.mikepenz.materialdrawer.util.getOptimalDrawerWidth
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var headerView: AccountHeaderView
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     companion object {
         fun getLaunchIntent(from: Context) = Intent(from, HomeActivity::class.java).apply {
             addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
         }
     }
+
 
 
     private lateinit var appBarConfiguration: AppBarConfiguration
@@ -29,32 +48,109 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-        toolbar.navigationIcon = getDrawable(R.drawable.ic_menu_camera)
+        toolbar.navigationIcon = getDrawable(R.drawable.ic_menu_white_24dp)
+        //val profile = ProfileDrawerItem().withName("Mike Penz").withEmail("mikepenz@gmail.com").withIcon(R.drawable.profile)
+//        val profile2 = ProfileDrawerItem().withName("Max Muster").withEmail("max.mustermann@gmail.com").withIcon(R.drawable.profile2)
+//        val profile3 = ProfileDrawerItem().withName("Felix House").withEmail("felix.house@gmail.com").withIcon(R.drawable.profile3)
+//        val profile4 = ProfileDrawerItem().withName("Mr. X").withEmail("mister.x.super@gmail.com").withIcon(R.drawable.profile4)
+//        val profile5 = ProfileDrawerItem().withName("Batman").withEmail("batman@gmail.com").withIcon(R.drawable.profile5)
+
+
+        val profile = ProfileDrawerItem().apply {
+            name = StringHolder("Muhamad Alfi")
+            description = StringHolder("alfi@gmail.com")
+            icon = ImageHolder(R.drawable.profile)
+
+        }
 
         val item1 = PrimaryDrawerItem()
         item1.run {
             identifier = 1
             name = StringHolder(R.string.menu_home)
             textColor = ColorHolder.fromColorRes(R.color.mainWhite)
-        }
-        val item2 = SecondaryDrawerItem()
-        item2.run {
-            identifier = 2
-            name = StringHolder(R.string.menu_gallery)
-            textColor = ColorHolder.fromColorRes(R.color.mainWhite)
+            icon = ImageHolder(R.drawable.ico_home_30px)
         }
 
-        slider.itemAdapter.add(
-            item1,
-            item2
-        )
-        toolbar.setNavigationOnClickListener {
-            slider.drawerLayout?.openDrawer(slider)
+        val item2= PrimaryDrawerItem()
+        item2.run {
+            identifier = 2
+            name = StringHolder(R.string.text_statistik)
+            textColor = ColorHolder.fromColorRes(R.color.mainWhite)
+            icon = ImageHolder(R.drawable.ico_futures_30px)
         }
-        slider.onDrawerItemClickListener = { _, item, position ->
-            println("Item Clicked $item $position ")
-            false
+        val item3= PrimaryDrawerItem()
+        item3.run {
+            identifier = 3
+            name = StringHolder(R.string.text_profile)
+            textColor = ColorHolder.fromColorRes(R.color.mainWhite)
+            icon = ImageHolder(R.drawable.ico_user_account_30px)
         }
+
+
+        headerView = AccountHeaderView(this).apply {
+            attachToSliderView(crossFadeLargeView)
+            headerBackground = ImageHolder(R.drawable.header)
+            addProfiles(
+                profile
+            )
+            withSavedInstance(savedInstanceState)
+        }
+
+        crossFadeLargeView.apply {
+            itemAdapter.add(
+                item1,item2,item3
+            )
+            addStickyDrawerItems(
+                SecondaryDrawerItem().withName(R.string.drawer_logout).withIcon(R.drawable.drawer_logout).withIdentifier(4)
+            )
+            onDrawerItemClickListener = { _, item, position ->
+                println("Item Clicked $item $position ")
+                false
+            }
+
+        }
+
+        crossFadeSmallView.drawer = crossFadeLargeView
+        drawer_layout.maxWidthPx = getOptimalDrawerWidth(this)
+        crossFadeSmallView.background = crossFadeLargeView.background
+
+        crossFadeSmallView.crossFader = object : ICrossfader {
+
+            override val isCrossfaded: Boolean
+                get() = drawer_layout.isCrossfaded
+
+            override fun crossfade() {
+                val isFaded = isCrossfaded
+                drawer_layout.crossfade(400)
+
+                //only close the drawer if we were already faded and want to close it now
+                if (isFaded) {
+                    drawer_layout.closeDrawer(GravityCompat.START)
+                }
+            }
+        }
+
+//        val item1 = PrimaryDrawerItem()
+//        item1.run {
+//            identifier = 1
+//            name = StringHolder(R.string.menu_home)
+//            textColor = ColorHolder.fromColorRes(R.color.mainWhite)
+//        }
+//        val item2 = SecondaryDrawerItem()
+//        item2.run {
+//            identifier = 2
+//            name = StringHolder(R.string.menu_gallery)
+//            textColor = ColorHolder.fromColorRes(R.color.mainWhite)
+//        }
+
+//        itemAdapter.add(
+//            item1,
+//            item2
+//        )
+//        toolbar.setNavigationOnClickListener {
+//            slider.drawerLayout?.openDrawer(slider)
+//        }
+
 
 
 //         Gada FindViewById, kecuali buat menu item
@@ -73,6 +169,41 @@ class HomeActivity : AppCompatActivity() {
 //        navView.setupWithNavController(navController)
     }
 
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        actionBarDrawerToggle.onConfigurationChanged(newConfig)
+    }
+
+//    override fun onPostCreate(savedInstanceState: Bundle?) {
+//        super.onPostCreate(savedInstanceState)
+//        actionBarDrawerToggle.syncState()
+//    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onSaveInstanceState(_outState: Bundle) {
+        var outState = _outState
+        //add the values which need to be saved from the drawer to the bundle
+        outState = crossFadeLargeView.saveInstanceState(outState)
+
+        //add the values which need to be saved from the accountHeader to the bundle
+        outState = headerView.saveInstanceState(outState)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onBackPressed() {
+        //handle the back press :D close the drawer first and if the drawer is closed close the activity
+        if (drawer_layout.isDrawerOpen(crossFadeSlider)) {
+            drawer_layout.closeDrawer(crossFadeSlider)
+        } else {
+            super.onBackPressed()
+        }
+    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
