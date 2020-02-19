758606afbcafdbabc231dd0a47cb06bd53b6f11a
Haikal Firdaus <haikal.firdaus.m@gmail.com>
2020-02-20 03:10:12 +0700

    Week&Month Statistics

--- app/src/main/java/com/hadir/hadirapp/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/hadir/hadirapp/ui/statistics/StatisticsFragment.kt
@@ -5,12 +5,16 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.hadir.hadirapp.R
 import com.hadir.hadirapp.model.DailyDataModel
 import com.hadir.hadirapp.model.TeacherModel
+import com.hadir.hadirapp.utils.TeacherRepository
+import kotlinx.android.synthetic.main.statistics_fragment.*
 import org.joda.time.LocalDate
 import java.util.*
 
@@ -19,8 +23,51 @@ class StatisticsFragment : Fragment() {
         ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory(requireActivity().application)).get(StatisticsViewModel::class.java)
     }
 
+    private val NumKehInMonth = 14
+    private val NumKehInWeek = 4
+    private val NumIz = 0
+    private val NumSak = 0
+    private val curDate = LocalDate.now()
+
     companion object {
         fun newInstance() = StatisticsFragment()
+    }
+
+    fun countPercent() {
+        val spin_items = arrayOf("Per Week", "Per Month")
+        var arAdpt = ArrayAdapter(requireActivity(), R.layout.spin_items, spin_items)
+        arAdpt.setDropDownViewResource(R.layout.spin_dropdown)
+        spin_stat.adapter = arAdpt
+
+        spin_stat.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                val selectedItem = p0?.getItemAtPosition(p2).toString()
+                if (selectedItem == "Per Week"){
+                    var totalWorkWeek = TeacherRepository().getWorkDayPerWeek(curDate)
+                    var percentage = (NumKehInWeek/totalWorkWeek) * 100
+                    circularProgress.apply {
+                        setProgressWithAnimation(percentage.toFloat(), 2000)
+                    }
+                    tvPersentage.setText(percentage.toString())
+                    text_statistic.setText("Statistics This Week")
+                }
+                if (selectedItem == "Per Month") {
+                    var totalWorkMonth = TeacherRepository().getWorkDayPerMonth(curDate)
+                    var percentage = (NumKehInMonth/totalWorkMonth) * 100
+                    circularProgress.apply {
+                        setProgressWithAnimation(percentage.toFloat(), 2000)
+                    }
+                    tvPersentage.setText(percentage.toString())
+                    text_statistic.setText("Statistics This Month")
+
+                }else{}
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
+
+        angka_kehadiran.setText(NumSak.toString())
+        angka_izin.setText(NumIz.toString())
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -36,6 +83,9 @@ class StatisticsFragment : Fragment() {
         vm.getDailyData(viewLifecycleOwner, Date()).observe(viewLifecycleOwner, Observer<ArrayList<DailyDataModel>> {
             Log.d("TEACHER_LIST_DAILY", "onActivityCreated: $it")
         })
-        vm.getPresentDataPerYear(viewLifecycleOwner, LocalDate(Date()))
+        vm.getPresentDataPerYear(viewLifecycleOwner, LocalDate(Date())).observe(viewLifecycleOwner, Observer<ArrayList<DailyDataModel>> {
+            Log.d("TEACHER_YEARLY", "onActivityCreated: $it")
+        })
+        countPercent()
     }
 }
